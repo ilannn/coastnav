095701bf68d0cffd42e1780235efccae9acc1727
Ilan Pe'er <ilan.peer@benextit.com>
2019-01-12 21:16:36 +0200

    Added custom arrow icon to TB step as a POC

--- src/map/steps/navStep/TBStep.jsx
+++ src/map/steps/navStep/TBStep.jsx
@@ -1,11 +1,22 @@
 import L from 'leaflet';
 
+const greenIcon = L.icon({
+    iconUrl: 'makefg.png',
+    //shadowUrl: 'leaf-shadow.png',
+
+    iconSize: [40, 40], // size of the icon
+    //shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [20, 20], // point of the icon which will correspond to marker's location
+    //shadowAnchor: [4, 62],  // the same for the shadow
+    //popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 const tbStepProps = {
     color: 'black',
     width: 8,
 }
 const tbMarkerProps = {
     // Put marker's const props here..
+    icon: greenIcon,
 }
 
 export default class TBStep {
