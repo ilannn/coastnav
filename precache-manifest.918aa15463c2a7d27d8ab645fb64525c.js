698f8facbf6d410ea901d253fbdce85e278f921f
Ilan Pe'er <just.ilann@gmail.com>
2020-07-09 00:52:43 +0300

    Updates

---
+++ precache-manifest.918aa15463c2a7d27d8ab645fb64525c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc2c07f7282cc03bddde",
+    "url": "/ilannn/coastnav/static/js/runtime~main.fc2c07f7.js"
+  },
+  {
+    "revision": "85f23bcc7d775ae80f03",
+    "url": "/ilannn/coastnav/static/js/main.85f23bcc.chunk.js"
+  },
+  {
+    "revision": "a5cd42466d930541e687",
+    "url": "/ilannn/coastnav/static/js/2.a5cd4246.chunk.js"
+  },
+  {
+    "revision": "85f23bcc7d775ae80f03",
+    "url": "/ilannn/coastnav/static/css/main.7f0f73de.chunk.css"
+  },
+  {
+    "revision": "04021db1ff2258567b27211c7748433b",
+    "url": "/ilannn/coastnav/index.html"
+  }
+];
